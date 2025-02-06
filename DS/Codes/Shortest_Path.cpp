2e839295693ed30f5b49c1a901645aac5263d45f
prajwalsin <61048832+prajwalsin@users.noreply.github.com>
2025-02-07 00:35:09 +0530

    Update Shortest_Path.cpp

--- DS/Codes/Shortest_Path.cpp
+++ DS/Codes/Shortest_Path.cpp
@@ -35,10 +35,51 @@ void djikstra(ll source,ll nodes){
 }
 
 void bellmanFord(){
+   // useful in directed graphs with negative edge weight but non negative cycles
+   // non negative cycle means the one can make infinite rounds of that bringing down the ans
 
+    ll dis[n+1];
+    ll lim = 1e12;
+    for(ll i=1;i<=n;i++){
+        if(i==source) dis[i]=0;
+        else          dis[i]=lim;
+    }
+    for(int i=1;i<n;i++){
+       for(int node=1;node<=n;node++){
+           if(dis[node] == lim) continue;
+           for(auto z:v[node]){
+               if((dis[node] + z.second) < dis[z.first])  dis[z.first] = dis[node] + z.second;
+           }
+       }
+    }
 }
 
 void floyWarshal(){
-
+   // Used tp get shortest distance between any two nodes in a
+   // 1) Undirected graph with non - negative edges
+   // 2) directed graph with non - negative cycle
+   ll dp[n+1][n+1];
+   ll lim = 1e12;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j != i) dp[i][j] = lim;
+            else       dp[i][j] = 0;
+        }
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(auto z:v[i]) dp[i][z.first] = z.second; 
+    }
+   
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(dp[i][k] < lim && dp[k][j] < lim){
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+    }
 }
 
